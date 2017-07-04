5bcb9f14efde5fd81a5f773dc3536da82e3db4e5
Victor Nikiforov <victor.nike@gmail.com>
2017-07-04 18:37:36 +0300

    Update - Use EBean Agent 10.3.1

--- src/main/groovy/io/ebean/gradle/EbeanEnhancer.groovy
+++ src/main/groovy/io/ebean/gradle/EbeanEnhancer.groovy
@@ -1,11 +1,8 @@
 package io.ebean.gradle
 
-import io.ebean.enhance.agent.InputStreamTransform
-import io.ebean.enhance.agent.Transformer
+import io.ebean.enhance.Transformer
+import io.ebean.enhance.common.InputStreamTransform
 import io.ebean.gradle.util.ClassUtils
-import io.ebean.gradle.util.EnhancementFileFilter
-import io.ebean.typequery.agent.CombinedTransform
-import io.ebean.typequery.agent.QueryBeanTransformer
 import org.gradle.api.logging.Logger
 import org.gradle.api.logging.Logging
 
@@ -21,87 +18,87 @@ class EbeanEnhancer {
      */
     private final Path outputDir
 
-    private final FileFilter fileFilter
+//    private final FileFilter fileFilter
 
-    private final CombinedTransform combinedTransform;
+    private final Transformer combinedTransform;
 
     private final ClassLoader classLoader;
 
-    EbeanEnhancer(Path outputDir, URL[] extraClassPath, ClassLoader classLoader, EnhancePluginExtension params) {
 
+    EbeanEnhancer(Path outputDir, URL[] extraClassPath, ClassLoader contextLoader, EnhancePluginExtension params) {
+
+        logger.info('Calling enhancer' + outputDir + ':' + extraClassPath)
         this.outputDir = outputDir
-        this.fileFilter = new EnhancementFileFilter(outputDir, params.packages)
-        this.classLoader = new URLClassLoader(extraClassPath, classLoader);
+        this.classLoader = new URLClassLoader(extraClassPath, contextLoader)
 
-        def args = "debug=" + params.debugLevel;
-
-        def packages = new HashSet<String>()
-        packages.addAll(params.packages)
-
-        def queryBeanTransformer = new QueryBeanTransformer(args, classLoader, packages);
-        def transformer = new Transformer(extraClassPath, args);
-
-        this.combinedTransform = new CombinedTransform(transformer, queryBeanTransformer);
+        def args = "debug=" + params.debugLevel
+        this.combinedTransform = new Transformer(classLoader, args)
     }
 
     void enhance() {
-
         collectClassFiles(outputDir.toFile()).each { classFile ->
-            if (fileFilter.accept(classFile)) {
-                enhanceClassFile(classFile);
-            }
+            enhanceClassFile(classFile)
         }
     }
 
     private void enhanceClassFile(File classFile) {
 
-        if (logger.isTraceEnabled()) {
-            logger.trace("trying to enhance $classFile.absolutePath")
+        def className = ClassUtils.makeClassName(outputDir, classFile)
+        if (isIgnorableClass(className)) {
+            return
         }
-
-        def className = ClassUtils.makeClassName(outputDir, classFile);
-
-        if (
-            className.contains('$$anonfun$') ||     //scala lambda: anonymous function
-            className.contains('$_')                //groovy meta info classes & closures
-        ) return
 
         try {
             classFile.withInputStream { classInputStream ->
 
                 def classBytes = InputStreamTransform.readBytes(classInputStream)
-                CombinedTransform.Response response = combinedTransform.transform(classLoader, className, null, null, classBytes)
 
-                if (response.isEnhanced()) {
+                // Make sure to close the stream otherwise classFile.delete() returns false on Windows
+                classInputStream.close()
+
+                String jvmClassName = className.replace('.','/')
+                byte[] response = combinedTransform.transform(classLoader, jvmClassName, null, null, classBytes)
+
+                if (response != null) {
                     try {
                         if (!classFile.delete()) {
                             logger.error("Failed to delete enhanced file at $classFile.absolutePath")
                         } else {
-                            InputStreamTransform.writeBytes(response.classBytes, classFile)
+                            InputStreamTransform.writeBytes(response, classFile)
                         }
 
                     } catch (IOException e) {
-                        throw new EnhanceException("Unable to store фт enhanced class data back to file $classFile.name", e);
+                        throw new EnhanceException("Unable to store enhanced class data back to file $classFile.name", e)
                     }
                 }
+//                else{
+//                    logger.error("File $jvmClassName is not enhanced");
+//                }
             }
         } catch (IOException e) {
-            throw new EnhanceException("Unable to read class file $classFile.name for enhancement", e);
+            throw new EnhanceException("Unable to read class file $classFile.name for enhancement", e)
         } catch (IllegalClassFormatException e) {
-            throw new EnhanceException("Unable to parse class file $classFile.name while enhance", e);
+            throw new EnhanceException("Unable to parse class file $classFile.name while enhance", e)
         }
+    }
+
+    /**
+     * Ignore scala lambda anonymous function and groovy meta info classes & closures
+     */
+    private static boolean isIgnorableClass(String className) {
+        return className.contains('$$anonfun$') || className.contains('$_')
     }
 
     private static List<File> collectClassFiles(File dir) {
 
-        List<File> classFiles = new ArrayList<>();
+        List<File> classFiles = new ArrayList<>()
 
         dir.listFiles().each { file ->
             if (file.directory) {
-                classFiles.addAll(collectClassFiles(file));
+                classFiles.addAll(collectClassFiles(file))
             } else {
                 if (file.name.endsWith(".class")) {
-                    classFiles.add(file);
+                    classFiles.add(file)
                 }
             }
         }
@@ -110,3 +107,95 @@ class EbeanEnhancer {
     }
 }
 
+//    EbeanEnhancer(Path outputDir, URL[] extraClassPath, ClassLoader contextLoader, EnhancePluginExtension params) {
+//
+//        this.outputDir = outputDir
+////        this.fileFilter = new EnhancementFileFilter(outputDir, params.packages)
+////        this.classLoader = new URLClassLoader(extraClassPath, contextLoader);
+//
+//        def args = "debug=" + params.debugLevel;
+//
+//        this.classLoader = new URLClassLoader(extraClassPath, contextLoader)
+//
+//        this.combinedTransform = new Transformer(this.classLoader, args)
+//
+//
+////        def packages = new HashSet<String>()
+////        packages.addAll(params.packages)
+////
+//////        def queryBeanTransformer = new QueryBeanTransformer(args, classLoader, packages);
+//////        def transformer = new Transformer(extraClassPath, args);
+////        this.transformer = new Transformer(extraClassPath, args);
+////
+////        this.combinedTransform = new CombinedTransform(transformer, queryBeanTransformer);
+//    }
+//
+//    void enhance() {
+//
+//        collectClassFiles(outputDir.toFile()).each { classFile ->
+//            enhanceClassFile(classFile)
+//        }
+////        collectClassFiles(outputDir.toFile()).each { classFile ->
+////            if (fileFilter.accept(classFile)) {
+////                enhanceClassFile(classFile);
+////            }
+////        }
+//    }
+//
+//    private void enhanceClassFile(File classFile) {
+//
+//        if (logger.isTraceEnabled()) {
+//            logger.trace("trying to enhance $classFile.absolutePath")
+//        }
+//
+//        def className = ClassUtils.makeClassName(outputDir, classFile);
+//
+//        if (
+//            className.contains('$$anonfun$') ||     //scala lambda: anonymous function
+//            className.contains('$_')                //groovy meta info classes & closures
+//        ) return
+//
+//        try {
+//            classFile.withInputStream { classInputStream ->
+//
+//                def classBytes = InputStreamTransform.readBytes(classInputStream)
+//                CombinedTransform.Response response = combinedTransform.transform(classLoader, className, null, null, classBytes)
+//
+//                if (response.isEnhanced()) {
+//                    try {
+//                        if (!classFile.delete()) {
+//                            logger.error("Failed to delete enhanced file at $classFile.absolutePath")
+//                        } else {
+//                            InputStreamTransform.writeBytes(response.classBytes, classFile)
+//                        }
+//
+//                    } catch (IOException e) {
+//                        throw new EnhanceException("Unable to store фт enhanced class data back to file $classFile.name", e);
+//                    }
+//                }
+//            }
+//        } catch (IOException e) {
+//            throw new EnhanceException("Unable to read class file $classFile.name for enhancement", e);
+//        } catch (IllegalClassFormatException e) {
+//            throw new EnhanceException("Unable to parse class file $classFile.name while enhance", e);
+//        }
+//    }
+//
+//    private static List<File> collectClassFiles(File dir) {
+//
+//        List<File> classFiles = new ArrayList<>();
+//
+//        dir.listFiles().each { file ->
+//            if (file.directory) {
+//                classFiles.addAll(collectClassFiles(file));
+//            } else {
+//                if (file.name.endsWith(".class")) {
+//                    classFiles.add(file);
+//                }
+//            }
+//        }
+//
+//        classFiles
+//    }
+//}
+//
